8e8369b50293854f1d509ea8e3cd59ecf4765096
andrew kerr <andrewkerr9000@gmail.com>
2013-07-11 18:11:14 +0100

    Replaced CopyOnWriteArrayList with a JsonArray wrapping an ArrayList with a size hint. As a verticle instance is guaranteed single threaded the overhead of CopyOnWriteArrayList is unnecessary. This may have contributed to relatively poor performance on multiple query benchmark.
    Introduced RouteMatcher.
    
    Removed direct use of Jackson, instead using JsonArray, JsonObject #encode().
    
    Used HttpServerResponse#end(String), explicit setting of content-length now unnecessary.
    
    Inlined DBHandler as anonymous class so can use closure.
    
    Added #setAcceptBacklog to the HttpServer.
    
    Minor refactorings.

--- vertx/WebServer.java
+++ vertx/WebServer.java
@@ -1,143 +1,94 @@
-import java.io.IOException;
-import java.nio.charset.*;
-import java.util.List;
-import java.util.Random;
-import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.concurrent.ThreadLocalRandom;
-import java.util.concurrent.atomic.AtomicInteger;
-
-import org.codehaus.jackson.map.ObjectMapper;
 import org.vertx.java.core.Handler;
 import org.vertx.java.core.eventbus.Message;
 import org.vertx.java.core.http.HttpServerRequest;
+import org.vertx.java.core.http.RouteMatcher;
 import org.vertx.java.core.json.JsonArray;
 import org.vertx.java.core.json.JsonObject;
 import org.vertx.java.deploy.Verticle;
 
-public class WebServer
-	extends    Verticle
-	implements Handler<HttpServerRequest>
-{
-  private final ObjectMapper mapper = new ObjectMapper();
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
+public class WebServer extends Verticle
+{
   @Override
   public void start() throws Exception
   {
-    this.getVertx().createHttpServer().requestHandler(this).listen(8080);
-  }
+    RouteMatcher routeMatcher = new RouteMatcher();
+    routeMatcher.get("/json", new Handler<HttpServerRequest>()
+    {
+      @Override
+      public void handle(HttpServerRequest httpServerRequest)
+      {
+        handleJson(httpServerRequest);
+      }
+    });
+    routeMatcher.get("/db", new Handler<HttpServerRequest>()
+    {
+      @Override
+      public void handle(HttpServerRequest httpServerRequest)
+      {
+        handleDb(httpServerRequest);
+      }
+    });
 
-  @Override
-  public void handle(HttpServerRequest req)
-  {
-    if (req.path.equals("/json"))
-    {
-      handleJson(req);
-    }
-    else if (req.path.equals("/db"))
-    {
-      handleDb(req);
-    }
-    else
-    {
-      req.response.statusCode = 404;
-      req.response.end();
-    }
-  }
-
-  public static class HelloMessage
-  {
-    public final String message = "Hello, world";
+    this.getVertx().createHttpServer().requestHandler(routeMatcher).setAcceptBacklog(10000).listen(8080);
   }
 
   private void handleJson(HttpServerRequest req)
   {
-    String result;
-    try
-    {
-      result = mapper.writeValueAsString(new HelloMessage());
-    }
-    catch (IOException e)
-    {
-      req.response.statusCode = 500;
-      req.response.end();
-      return;
-    }
-    
-    int contentLength = result.getBytes(StandardCharsets.UTF_8).length;
-    req.response.putHeader("Content-Type", "application/json; charset=UTF-8");
-    req.response.putHeader("Content-Length", contentLength);
-    req.response.write(result);
-    req.response.end();
+    JsonObject helloWorld = new JsonObject().putString("message", "Hello, world");
+    req.response.putHeader("Content-Type", "application/json; charset=UTF-8").end(helloWorld.encode());
   }
 
   private void handleDb(final HttpServerRequest req)
   {
-    int queriesParam = 1;
-    try 
-    {
-      queriesParam = Integer.parseInt(req.params().get("queries"));
-    }
-    catch (NumberFormatException e)
-    {
-      // do nothing
-    }
-
-    final DbHandler dbh = new DbHandler(req, queriesParam);
+    final int queriesParam = getQueries(req);
     final Random random = ThreadLocalRandom.current();
+    final JsonArray worlds = new JsonArray(new ArrayList<>(queriesParam));
 
     for (int i = 0; i < queriesParam; i++)
     {
+      JsonObject mongoQuery = new JsonObject()
+          .putString("action", "findone")
+          .putString("collection", "world")
+          .putObject("matcher", new JsonObject().putNumber("id", (random.nextInt(10000) + 1)));
+
       this.getVertx().eventBus().send(
-        "hello.persistor",
-        new JsonObject()
-            .putString("action", "findone")
-            .putString("collection", "world")
-            .putObject("matcher", new JsonObject().putNumber("id", (random.nextInt(10000) + 1))),
-        dbh);
+          "hello.persistor",
+          mongoQuery,
+          new Handler<Message<JsonObject>>()
+          {
+            @Override
+            public void handle(Message<JsonObject> reply)
+            {
+              final JsonObject body = reply.body;
+              if ("ok".equals(body.getString("status")))
+              {
+                worlds.addObject(body.getObject("result"));
+              }
+
+              if (worlds.size() == queriesParam)
+              {
+                // All queries have completed; send the response.
+                req.response.putHeader("Content-Type", "application/json; charset=UTF-8").end(worlds.encode());
+              }
+            }
+          }
+      );
     }
   }
 
-  class DbHandler implements Handler<Message<JsonObject>>
+  private int getQueries(HttpServerRequest req)
   {
-    private final HttpServerRequest req;
-    private final int queries;
-    private final List<Object> worlds = new CopyOnWriteArrayList<>();
-
-    public DbHandler(HttpServerRequest request, int queriesParam)
+    try
     {
-   	  this.req = request;
-      this.queries = queriesParam;
-    }
-
-    @Override
-    public void handle(Message<JsonObject> reply)
+      return Integer.parseInt(req.params().get("queries"));
+    } catch (NumberFormatException nfe)
     {
-      final JsonObject body = reply.body;
-
-      if ("ok".equals(body.getString("status")))
-      {
-      	this.worlds.add(body.getObject("result"));
-      }
-
-      if (this.worlds.size() == this.queries)
-      {
-        // All queries have completed; send the response.
-        // final JsonArray arr = new JsonArray(worlds);
-        try
-        {
-          final String result = mapper.writeValueAsString(worlds);
-          final int contentLength = result.getBytes(StandardCharsets.UTF_8).length;
-          this.req.response.putHeader("Content-Type", "application/json; charset=UTF-8");
-          this.req.response.putHeader("Content-Length", contentLength);
-          this.req.response.write(result);
-          this.req.response.end();
-        }
-        catch (IOException e)
-        {
-          req.response.statusCode = 500;
-          req.response.end();
-        }
-      }
+      return 1;
     }
   }
+
 }
