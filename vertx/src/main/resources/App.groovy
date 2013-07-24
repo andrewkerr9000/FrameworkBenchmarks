57f9711462bc14d8193b247c36aa84c7040eb5ec
andrew kerr <andrewkerr9000@gmail.com>
2013-07-24 11:22:05 +0100

    Adding comments

--- vertx/src/main/resources/App.groovy
+++ vertx/src/main/resources/App.groovy
@@ -8,11 +8,12 @@ def persistorConf = [
 
 container.with {
 
-    // Deploy the busmods
+    //modules and verticles are deployed asynchronously, and we want to know then they're all ready.
     deployModule('io.vertx~mod-mongo-persistor~2.0.0-final', persistorConf, 8) { persistorResult ->
         if (persistorResult.succeeded) {
             deployVerticle('hellovertx.WebServer', [:], 8){ webServerResult ->
                 if (webServerResult.succeeded) {
+                    // This magic string is looked for by the startup script to indicate that WebServer is ready to roll
                     println "The WebServer has been deployed, deployment ID is ${webServerResult.result}"
                 } else {
                     webServerResult.cause.printStackTrace()
@@ -22,8 +23,4 @@ container.with {
             persistorResult.cause.printStackTrace()
         }
     }
-
-    // Start the web server
-
-
 }
